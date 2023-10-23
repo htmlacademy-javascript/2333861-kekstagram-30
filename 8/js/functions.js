3dcb788ebdc7115e4e56cf4c0a03a7af8ee5257e
Keks <keks@htmlacademy.ru>
2023-10-23 11:46:46 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/functions.js
@@ -0,0 +1,35 @@
+const checkStringLength = (string, length) => {
+  if (string.length <= length) {
+    return true;
+  }
+
+  return false;
+};
+
+checkStringLength('как рабоатет этот линт', 30);
+
+const checkPalindrome = (string) => {
+  const fixString = string.replaceAll(' ', '').toLowerCase();
+  let compareStr = '';
+
+  for (let i = fixString.length - 1; i >= 0; i--) {
+    compareStr += fixString[i];
+  }
+
+  return fixString === compareStr;
+};
+checkPalindrome('шалаш');
+
+
+const getNumber = (string) => {
+  let parseString = '';
+  for (let i = 0; i < string.length; i++) {
+    const value = parseInt(string[i], 10);
+    if (!isNaN(value)) {
+      parseString += String(value);
+    }
+  }
+
+  return parseString ? Math.round(parseString) : NaN;
+};
+getNumber('верните 2007');
---
+++ 8/js/main.js
@@ -0,0 +1,3 @@
+import { photoCard } from './create-element';
+
+const allCards = Array.from({ length: 25 }, photoCard);
